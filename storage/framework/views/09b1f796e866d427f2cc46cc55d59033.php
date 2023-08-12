cd7a3a389511240457e9130288ba851a518c6aa5
Abdulahad Azimov <aabdulaxadkhan@gmail.com>
2023-08-12 21:53:39 +0500

    new style

--- storage/framework/views/09b1f796e866d427f2cc46cc55d59033.php
+++ storage/framework/views/09b1f796e866d427f2cc46cc55d59033.php
@@ -1,5 +1,5 @@
 <!-- start page title -->
-<div class="row">
+<!-- <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
             <h4 class="mb-sm-0 font-size-18"><?php echo e($title); ?></h4>
@@ -15,6 +15,6 @@
 
         </div>
     </div>
-</div>
+</div> -->
 <!-- end page title -->
 <?php /**PATH C:\OpenServer\domains\turonsoft.uz\resources\views/components/breadcrumb.blade.php ENDPATH**/ ?>
