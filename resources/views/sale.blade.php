f620e3e18f840e8da7b6dac491c6d1e8ec284d8f
Abdulahad Azimov <aabdulaxadkhan@gmail.com>
2023-09-29 23:43:08 +0500

    settinglarni to'g'rilab chiqdim

--- resources/views/sale.blade.php
+++ resources/views/sale.blade.php
@@ -1,23 +1,24 @@
-<?php $__env->startSection('title'); ?> <?php echo app('translator')->get('translation.storeTransition'); ?> <?php $__env->stopSection(); ?>
-<?php $__env->startSection('css'); ?>
+@extends('layouts.master')
+@section('title') @lang('translation.storeTransition') @endsection
+@section('css')
 <!--datatable css-->
-<link href="<?php echo e(URL::asset('build/css/dataTables.bootstrap5.min.css')); ?>" id="app-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('build/css/dataTables.bootstrap5.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 <!--datatable responsive css-->
-<link href="<?php echo e(URL::asset('build/css/responsive.bootstrap.min.css')); ?>" id="app-style" rel="stylesheet" type="text/css" />
-<link href="<?php echo e(URL::asset('build/css/buttons.dataTables.min.css')); ?>" id="app-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('build/css/responsive.bootstrap.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('build/css/buttons.dataTables.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
-<?php $__env->stopSection(); ?>
-<?php $__env->startSection('content'); ?>
-<?php $__env->startComponent('components.breadcrumb'); ?>
-<?php $__env->slot('li_1'); ?> Tables <?php $__env->endSlot(); ?>
-<?php $__env->slot('title'); ?>Datatables <?php $__env->endSlot(); ?>
-<?php echo $__env->renderComponent(); ?>
+@endsection
+@section('content')
+@component('components.breadcrumb')
+@slot('li_1') Tables @endslot
+@slot('title')Datatables @endslot
+@endcomponent
 
     <div class="row">
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header d-flex justify-content-between align-items-center">
-                    <h5 class="card-title mb-0"><?php echo app('translator')->get('translation.storeTransition'); ?></h5>
+                    <h5 class="card-title mb-0">@lang('translation.storeTransition')</h5>
                     <div class="d-flex">
                         <div class="d-flex ms-3">
                             <button
@@ -26,7 +27,7 @@
                                 type="button"
                                 class="btn btn-soft-success">
                                 <i class="ri-add-circle-line align-middle me-1"></i>
-                                <span><?php echo app('translator')->get('translation.cret'); ?></span>
+                                <span>@lang('translation.cret')</span>
                             </button>
                         </div>
                     </div>
@@ -41,11 +42,11 @@
                                     </div>
                                 </th>
                                 <th>No.</th>
-                                <th><?php echo app('translator')->get('translation.provider'); ?></th>
-                                <th><?php echo app('translator')->get('translation.consignment'); ?></th>
-                                <th><?php echo app('translator')->get('translation.settingCurrencies'); ?></th>
-                                <th><?php echo app('translator')->get('translation.settingCurrenciesKurs'); ?></th>
-                                <th><?php echo app('translator')->get('translation.totalSum'); ?></th>
+                                <th>@lang('translation.provider')</th>
+                                <th>@lang('translation.consignment')</th>
+                                <th>@lang('translation.settingCurrencies')</th>
+                                <th>@lang('translation.settingCurrenciesKurs')</th>
+                                <th>@lang('translation.totalSum')</th>
                                 <th>Create Date</th>
                                 <th>Action</th>
                             </tr>
@@ -100,7 +101,7 @@
                     <div class="live-preview">
                         <div class="row gy-4">
                             <div class="col-xxl-4 col-md-6 col-12 mb-3">
-                                <label for="hrRol" class="form-label"><?php echo app('translator')->get('translation.provider'); ?></label>
+                                <label for="hrRol" class="form-label">@lang('translation.provider')</label>
                                 <select class="form-control"
                                         id="choices-single-no-sorting"
                                         name="choices-single-no-sorting"
@@ -129,15 +130,15 @@
                              <!--end col-->
                             <div class="col-xxl-4 col-md-6 col-12  mb-3">
                                 <div>
-                                     <label for="consignment" class="form-label"><?php echo app('translator')->get('translation.consignment'); ?></label>
-                                    <input type="text" class="form-control" id="consignment"  placeholder="<?php echo app('translator')->get('translation.consignment'); ?>">
+                                     <label for="consignment" class="form-label">@lang('translation.consignment')</label>
+                                    <input type="text" class="form-control" id="consignment"  placeholder="@lang('translation.consignment')">
                                 </div>
                             </div>
                              <!--end col-->
                             <div class="col-xxl-4 col-md-6 col-12  mb-3">
                                 <div>
-                                    <label for="date"  class="form-label"><?php echo app('translator')->get('translation.date'); ?></label>
-                                    <input type="date" class="form-control" id="date" placeholder="<?php echo app('translator')->get('translation.date'); ?>">
+                                    <label for="date"  class="form-label">@lang('translation.date')</label>
+                                    <input type="date" class="form-control" id="date" placeholder="@lang('translation.date')">
                                 </div>
                             </div>
                              <!--end col-->
@@ -147,7 +148,7 @@
                         <div class="row gy-4">
                             <div class="col-xxl-4 col-md-6 col-12">
                                 <div>
-                                    <label for="hrRol" class="form-label"><?php echo app('translator')->get('translation.settingCurrencies'); ?></label>
+                                    <label for="hrRol" class="form-label">@lang('translation.settingCurrencies')</label>
                                     <select class="form-select mb-1" aria-label="Default select example">
                                         <option selected></option>
                                         <option value="1">One</option>
@@ -156,7 +157,7 @@
                                     </select>
                                 </div>
                                 <div>
-                                    <input type="text" class="form-control" id="userName" placeholder="<?php echo app('translator')->get('translation.settingCurrenciesKurs'); ?>">
+                                    <input type="text" class="form-control" id="userName" placeholder="@lang('translation.settingCurrenciesKurs')">
                                 </div>
                             </div>
                         </div>
@@ -165,7 +166,7 @@
                         <hr>
                         <div class="row gy-4">
                             <div class="col-xxl-9 col-12">
-                                <label for="hrRol" class="form-label"><?php echo app('translator')->get('translation.provider'); ?></label>
+                                <label for="hrRol" class="form-label">@lang('translation.provider')</label>
                                 <select placeholder="salom" class="form-control"
                                         id="choices-single-no-sorting"
                                         name="choices-single-no-sorting"
@@ -199,15 +200,15 @@
                             <thead>
                                 <tr>
                                     <th scope="col">Id</th>
-                                    <th scope="col"><?php echo app('translator')->get('translation.provider'); ?></th>
-                                    <th scope="col" style="width: 100px;"><?php echo app('translator')->get('translation.settingMeasurement'); ?></th>
-                                    <th scope="col" style="width: 150px;"><?php echo app('translator')->get('translation.miqdorMin'); ?></th>
-                                    <th scope="col"><?php echo app('translator')->get('translation.settingWarehouses'); ?></th>
-                                    <th scope="col" style="width: 200px;"><?php echo app('translator')->get('translation.expenses'); ?> %</th>
-                                    <th scope="col"><?php echo app('translator')->get('translation.arrivalPrice'); ?></th>
-                                    <th scope="col"><?php echo app('translator')->get('translation.arrivalPrice'); ?>, <?php echo app('translator')->get('translation.expenses'); ?></th>
-                                    <th scope="col"><?php echo app('translator')->get('translation.typeSale'); ?></th>
-                                    <th scope="col"><?php echo app('translator')->get('translation.saleProduct'); ?></th>
+                                    <th scope="col">@lang('translation.provider')</th>
+                                    <th scope="col" style="width: 100px;">@lang('translation.settingMeasurement')</th>
+                                    <th scope="col" style="width: 150px;">@lang('translation.miqdorMin')</th>
+                                    <th scope="col">@lang('translation.settingWarehouses')</th>
+                                    <th scope="col" style="width: 200px;">@lang('translation.expenses') %</th>
+                                    <th scope="col">@lang('translation.arrivalPrice')</th>
+                                    <th scope="col">@lang('translation.arrivalPrice'), @lang('translation.expenses')</th>
+                                    <th scope="col">@lang('translation.typeSale')</th>
+                                    <th scope="col">@lang('translation.saleProduct')</th>
                                     <th scope="col"></th>
                                 </tr>
                             </thead>
@@ -266,24 +267,22 @@
     </div>
 
 
-<?php $__env->stopSection(); ?>
-<?php $__env->startSection('script'); ?>
+@endsection
+@section('script')
 
-<script src="<?php echo e(URL::asset('build/js/pages/jquery-3.6.0.min.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/pages/dataTables.min.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/pages/dataTables.bootstrap5.min.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/pages/dataTables.responsive.min.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/pages/dataTables.buttons.min.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/pages/buttons.print.min.js')); ?>"></script>
+<script src="{{ URL::asset('build/js/pages/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ URL::asset('build/js/pages/dataTables.min.js') }}"></script>
+<script src="{{ URL::asset('build/js/pages/dataTables.bootstrap5.min.js') }}"></script>
+<script src="{{ URL::asset('build/js/pages/dataTables.responsive.min.js') }}"></script>
+<script src="{{ URL::asset('build/js/pages/dataTables.buttons.min.js') }}"></script>
+<script src="{{ URL::asset('build/js/pages/buttons.print.min.js') }}"></script>
 <!-- <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script> -->
 
-<script src="<?php echo e(URL::asset('build/js/pages/datatables.init.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/libs/prismjs/prism.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/app.js')); ?>"></script>
-<?php $__env->stopSection(); ?>
+<script src="{{ URL::asset('build/js/pages/datatables.init.js') }}"></script>
+<script src="{{ URL::asset('build/libs/prismjs/prism.js') }}"></script>
+<script src="{{ URL::asset('build/js/app.js') }}"></script>
+@endsection
 
-
-<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\OpenServer\domains\turonsoft.uz\resources\views/store-transition.blade.php ENDPATH**/ ?>
